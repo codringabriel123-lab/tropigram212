3d2b8f515c0874feb4ad271ab4d7a473c86adfc3
codringabriel123-lab <codringabriel123@gmail.com>
2026-06-26 01:22:09 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(cors({
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 200 });
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 2000 });
 app.use(limiter);
 
 mongoose.connect(process.env.MONGO_URI)
